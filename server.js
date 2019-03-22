f5ce3e780ad0b36c09d497fc70dddae9429900fc
dislersd <dislersd@gmail.com>
2019-03-22 08:35:24 -0700

    imported routes to server

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const express = require('express');
+// express
+const express = require("express");
 const server = express();
+// routes
+const actionRouter = require("./Routers/actionRouter.js");
+const projectRouter = require("./Routers/projectRouter.js");
 
 server.use(express.json());
+server.use("/actions", actionRouter);
+server.use("/projects", projectRouter);
 
-server.get('/', (req, res) => {
-  res.status(200).send('Hello World')
+server.get("/", async (req, res) => {
+  try {
+    res.status(200).send("Hello World");
+  } catch (error) {
+    res.status(500).json({ message: "error loading page" });
+  }
 });
 
 module.exports = server;
