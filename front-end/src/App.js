9d25c6c9d7be22a7eb597966b7215e46c138e523
dislersd <dislersd@gmail.com>
2019-03-22 12:30:18 -0700

    added delete function in front end to remove projects

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,30 +1,41 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 import "./App.css";
 
 function App() {
-  
-  const [projects, setProjects] = useState([])
+  const [projects, setProjects] = useState([]);
 
-  useEffect( () => {
+  useEffect(() => {
     const fetchProjects = async () => {
-      const res = await axios.get('http://localhost:3000/projects')
-      console.log(res);
-      setProjects(res.data)
-    }
+      const res = await axios.get("http://localhost:3000/projects");
+      setProjects(res.data);
+      console.log("useEffect ran");
+    };
     fetchProjects();
-  },[])
+  }, []);
 
-  return (
-  <div className="App">
-    {projects.map(project => (
-      <div key={project.key}>
-        <p>{project.name}</p>
-        <p>{project.description}</p>
-      </div>
-    ))}
-  </div>
-  )
+
+  async function removeProject(id) {
+    await axios.delete(`http://localhost:3000/projects/${id}`);
+    console.log('delete ran');
+    const res = await axios.get("http://localhost:3000/projects")
+    setProjects(res.data)
+  }
+
+  return projects.length === 0 ? (
+    <div>Loading...</div>
+  ) : (
+    <div className="App">
+      {projects.map(project => (
+        <div key={project.id}>
+          <p>Project: {project.name}</p>
+          <p>Description: {project.description}</p>
+          <button onClick={() => removeProject(project.id)}> x </button>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
